864cf8b7c8a1db2732f10a2c83bec742bf42e20b
eliasjr <elias.dvlpr@gmail.com>
2019-12-23 16:14:10 -0400

    class produto

--- src/main/java/br/com/icts/flex/wsvendasapi/model/Produto.java
+++ src/main/java/br/com/icts/flex/wsvendasapi/model/Produto.java
@@ -1,5 +1,80 @@
 package br.com.icts.flex.wsvendasapi.model;
 
-public class Produto {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = -4513299829436850638L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "titulo")
+	private String titulo;
+
+	@Column(name = "descricao")
+	private String descricao;
+
+	@Column(name = "valor")
+	private float valor;
+
+	@Column(name = "status")
+	private String status;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", valor=" + valor
+				+ ", status=" + status + "]";
+	}
 
 }
